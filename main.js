4e265f6b8c16068034ac2fd81536c6784238d0a0
Gaspare <Gas9220@users.noreply.github.com>
2023-04-12 17:10:26 +0200

    Added text decoration for completed todos

--- main.js
+++ main.js
@@ -21,6 +21,9 @@ createApp({
                     this.todos = response.data;
                     console.log(this.todos)
                 })
+        },
+        todoStatus(todo) {
+            return todo.done ? "Completed" : "To complete"
         }
     },
     created() {
